82d6a8c8679054a247e85281c45f27b9e0970e97
luidy <luidyc@gmail.com>
2025-01-26 11:39:57 -0300

    Run : Make changes to deploy

--- src/app/services/notas/notas.service.ts
+++ src/app/services/notas/notas.service.ts
@@ -1,21 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NotasResponse } from '../../types/notas-response.type';
 import { Observable, tap } from 'rxjs';
 import { ItemNota } from '../../types/itemnota-response.type';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotasService {
-  apiUrl : string = "http:localhost:8080/notas"
+  apiUrl : string = "http://localhost:8080/NotaFiscal"
   constructor(private httpClient:HttpClient) {}
 
-  create(numeroNota:string, itens:ItemNota[]):Observable<NotasResponse> {
-    return this.httpClient.post<NotasResponse>
-    (this.apiUrl,{numeroNota,itens}).pipe(
+  create(notasForm:FormGroup, itens:ItemNota[]):Observable<NotasResponse> {
+    const token = sessionStorage.getItem("auth-token")
+    const headers = new HttpHeaders().set("Authorization",`Bearer ${token}`)
+    return this.httpClient.post<NotasResponse>(
+      this.apiUrl,
+      {
+        numeroNota:notasForm.get("numeroNota")?.value,
+        transporte:{id:notasForm.get("transporte.id")?.value},
+        itens
+      },{headers}).pipe(
       tap((value)=> {
         sessionStorage.setItem("numeroNota",value.numeroNota)
+        sessionStorage.setItem("id",value.transporte.id.toString())
         sessionStorage.setItem("listaItemNota",JSON.stringify(value.itens))
       }
     ))
